df1c2f20995b6fec3f827ebd7185aba9a350e2b7
lijunlhc <lijunlhc@gmail.com>
2018-12-24 19:44:42 +0800

    modified

---
+++ Chapter_17/17_8_use_q.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "17_6_queue.h"
+int main()
+{
+	Queue line;
+	Item temp;
+	char ch;
+
+	InitializeQueue(&line);
+	puts("Testing the Queue interface. Type a to add a value, ");
+	puts("type d to delete a value, and type q to quit.");
+	while((ch = getchar()) != 'q')
+	{
+		if(ch != 'a' && ch != 'd')
+			continue;
+		if(ch == 'a')
+		{
+			printf("Integer to add: ");
+			scanf("%d", &temp);
+			if(!QueueIsFull(&line))
+			{
+				printf("Putting %d into queue\n", temp);
+				EnQueue(temp, &line);
+			}
+			else
+				puts("Queue is full!");
+		}
+		else
+		{
+			if(QueueIsEmpty(&line))
+				puts("Nothing to delete!");
+			else
+			{
+				DeQueue(&temp, &line);
+				printf("Removing %d from queue\n", temp);
+			}
+		}
+		printf("%d items in queue\n", QueueItemCount(&line));
+		puts("Type a to add, d to delete, q to quit: ");
+	}
+	EmptyTheQueue(&line);
+	puts("Bye!");
+
+	return 0;
+}
